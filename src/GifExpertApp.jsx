86913d4fbb2087d5d82042542df98102dc2b4849
LENOVO\jgutierrez12 <jgutierrez12@lenovo.com>
2023-11-14 15:46:33 -0600

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['dbz']);
+
+    const onAddCategory = ( newCategory ) => {
+        
+        if ( categories.includes( newCategory ) ) return;
+        setCategories([ newCategory, ...categories ]);
+        // setCategories( cat => [ ...cat, 'Valorant' ] );
+    }
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>  
+
+            <AddCategory 
+                // onAddCategory={ setCategories }
+                onNewCategory={ (value) => onAddCategory(value) }
+            />
+            
+            { 
+                categories.map( category => (
+                    <GifGrid key={ category } category={ category } />
+                )) 
+            }     
+        </>
+    )
+}
